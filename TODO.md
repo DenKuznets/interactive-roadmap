d86f377ef2584f91f4ac025d125b1e633a65e5b8
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-03-01 12:48:04 +0300

    savetolocalstorage

--- TODO.md
+++ TODO.md
@@ -1,3 +1,2 @@
-empty text field
-save text to localStorage
+
 save text to json on user pc
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-
-import './App.css'
-import Textfield from './components/Textfield'
-import Button from './components/Button'
+import "./App.css";
+import Textfield from "./components/Textfield";
+import Button from "./components/Button";
 
 function App() {
-
   function saveToLocalStorage() {
+    // найти все textfield
+    const textFields = document.querySelectorAll("textarea");
+    // считать текст из них
+    for (let tf of textFields) {
+      localStorage.setItem('textfieldId1', tf.value);
+      console.log(localStorage.getItem("textfieldId1"));
+    }
+  }
+
+  function saveToJSON() {
     
   }
 
   return (
     <div className="App">
-      <Button text="save to localStorage" />
-      <br/>
+      <Button onClick={saveToLocalStorage} text="save to localStorage" />
+      <Button onClick={saveToLocalStorage} text="save to JSON" />
+      <br />
       <Textfield />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
