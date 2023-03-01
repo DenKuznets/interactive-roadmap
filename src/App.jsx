8ba31d3dcd4bdb5420c5944cbc39491b306f9a72
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-03-01 15:43:15 +0300

    saveToLocalStorage refactored

--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,56 @@ import MapNode from "./components/MapNode";
 import Button from "./components/Button";
 import { useState } from "react";
 import { nanoid } from "nanoid";
+import { _ } from "lodash";
 
 function App() {
+  const [downloadLink, setDownloadLink] = useState("");
+  const [mapNodesState, setMapNodesState] = useState(() => [
+    {
+      text: "lalala",
+      id: nanoid(),
+      connections: [],
+    },
+  ]);
 
-  const [downloadLink, setDownloadLink] = useState('');
-  const [mapNodesState, setMapNodesState] = useState(() => [{
-    text: "lalala",
-    id: nanoid(),
-    connections: [],
-  }]);
-
-  const elems = mapNodesState.map(elem => <MapNode mapNodeObj={elem} onChange={onTextAreaTextChange} key={elem.id} />);
-
+  const elems = mapNodesState.map((elem) => (
+    <MapNode mapNodeObj={elem} onChange={onTextAreaTextChange} key={elem.id} />
+  ));
 
   function onTextAreaTextChange(e) {
-    setMapNodesState(prev => {
-      return prev.map(elem => {
+    setMapNodesState((prev) => {
+      return prev.map((elem) => {
         return {
           ...elem,
           text: e.target.value,
-        }
-      })
-    })
+        };
+      });
+    });
   }
 
   function saveToLocalStorage() {
-    // найти все textfield
-    const textFields = document.querySelectorAll("textarea");
-    // считать текст из них
-    for (let tf of textFields) {
-      let text = tf.value;
-      localStorage.setItem('textfieldId1', text);
-      console.log(localStorage.getItem("textfieldId1"));
-    }
+    let json = JSON.stringify(mapNodesState);
+    localStorage.setItem("roadmapState", json);
+    if (
+      _.isEqual(JSON.parse(localStorage.getItem("roadmapState")), mapNodesState)
+    )
+      console.log("saved to localStorage");
   }
 
-  function createBackup() {
-    
-  }
+  function createBackup() {}
 
   function saveToJSON() {
     // найти все textfield
     const textFields = document.querySelectorAll("textarea");
-    
+
     // считать текст из них
     for (let tf of textFields) {
       let userText = tf.value;
       let obj = {
         text: userText,
-      }
+      };
       let json = JSON.stringify(obj);
-      let blob = new Blob([json], { type: 'application/json' });
+      let blob = new Blob([json], { type: "application/json" });
       let url = URL.createObjectURL(blob);
       let link = (
         <a download={"backup.json"} href={url}>
@@ -64,7 +63,6 @@ function App() {
       setDownloadLink(link);
     }
   }
-
 
   return (
     <div className="App">
